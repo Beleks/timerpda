4f97a1aa01fd3fb67a43ea0a473884ec38011ad1
Aleksey <53120159+Beleks@users.noreply.github.com>
2021-01-16 10:44:07 +0700

    Add files via upload

---
+++ precache-manifest.83ea7be0fd0f061dc0b6d42c01167971.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f01d199c0f88d1b70b",
+    "url": "/css/app.dfbd1420.css"
+  },
+  {
+    "revision": "770108b4e177f8155466",
+    "url": "/css/chunk-32db4704.db38fa85.css"
+  },
+  {
+    "revision": "c51019eac7039adcdd5faebdfc2a0b05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94f01d199c0f88d1b70b",
+    "url": "/js/app.272fe680.js"
+  },
+  {
+    "revision": "770108b4e177f8155466",
+    "url": "/js/chunk-32db4704.86a79951.js"
+  },
+  {
+    "revision": "5d3808f0e5a00dbceb82",
+    "url": "/js/chunk-vendors.21b6ddc0.js"
+  },
+  {
+    "revision": "c04c1dd6ea3bbaa5b69a1fc1f11a4265",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "94c4437b52c34b3852796df929beef32",
+    "url": "/media/exh.94c4437b.mp3"
+  },
+  {
+    "revision": "54314cad142510b83072b386e4b4db43",
+    "url": "/media/inh.54314cad.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
